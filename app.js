983d8a45670c71fe4a62d90795e1b101b484ef98
Pauli019 <paulimonjes@gmail.com>
2022-02-07 18:17:35 -0300

    Agregué home y carrito casi terminados, sin imagenes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 
-constpath = require('path');
+const path = require('path');
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/index.html')));
 
 app.listen(3000, ()=> console.log('servidor corriendo en el puerto 3000'));
 
