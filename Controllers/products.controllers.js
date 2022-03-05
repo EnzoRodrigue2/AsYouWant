9d5f9f2cef1147b476adeebde12b2a9e77261bf9
Adriel2323 <adrielcolombo23@gmail.com>
2022-03-05 19:58:40 -0300

    Termine rutas parametrizadas y pagina info productos

--- Controllers/products.controllers.js
+++ Controllers/products.controllers.js
@@ -8,8 +8,9 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-const searchForId = (id) => { products.filter(products => products.id == id)
-    return products
+const searchForId = (id) => { 
+    let productos = products.filter(products => products.id == id);
+    return productos
 };
 
 
