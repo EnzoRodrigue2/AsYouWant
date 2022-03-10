6167a7676ef2f3c43423b1521f483c5597ed7a17
Pauli019 <paulimonjes@gmail.com>
2022-03-10 01:02:45 -0300

    agregué algo del código del carrusel pero no me salió (todavía)

--- Routes/main.routes.js
+++ Routes/main.routes.js
@@ -5,4 +5,10 @@ const mainController = require('../Controllers/main.controllers')
 /* GET home page. */
 router.get('/', mainController.home);
 
+/*GET crear producto*/
+router.get('/crear', mainController.crear);
+
+/*GET editar producto*/
+router.get('/editar', mainController.editar);
+
 module.exports = router;
