6bbffd90016a45466a586fdf6ed9c6b8d3fd3552
Pauli019 <paulimonjes@gmail.com>
2022-03-20 16:49:48 -0300

    cambié el carrusel por el banner, eliminé rutas repetidas

--- Controllers/main.controllers.js
+++ Controllers/main.controllers.js
@@ -2,12 +2,6 @@ const controller = {
     home :  function(req, res, next) {
       res.render('index', { title: 'Express' });
     },
-    crear : function(req, res, next) {
-      res.render('crear-producto');
-    },
-    editar : function(req, res, next) {
-      res.render('editar-producto');
-    }
 };
 
 module.exports = controller
--- Routes/main.routes.js
+++ Routes/main.routes.js
@@ -5,10 +5,5 @@ const mainController = require('../Controllers/main.controllers')
 /* GET home page. */
 router.get('/', mainController.home);
 
-/*GET crear producto*/
-/*router.get('/crear', mainController.crear);
-
-/*GET editar producto*/
-router.get('/editar', mainController.editar);
 
 module.exports = router;
