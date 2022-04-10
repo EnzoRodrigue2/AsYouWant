ad76f84a8cc6e4f4e6f869a1617a57d11b8d0c5d
Pauli019 <paulimonjes@gmail.com>
2022-04-09 23:54:57 -0300

    Arreglé el home, agregué profesores al json de cursos

--- Controllers/main.controllers.js
+++ Controllers/main.controllers.js
@@ -1,6 +1,13 @@
+const path = require('path');
+const fs = require('fs');
+
+const cursosPath = path.join(__dirname, '../data/cursosDataBase.json');
+const cursos = JSON.parse(fs.readFileSync(cursosPath, 'utf-8'));
+
+
 const controller = {
     home :  function(req, res, next) {
-      res.render('index', { title: 'Express' });
+      res.render('index', { cursos: cursos });
     },
     carrito : function(req, res, next) {
       res.render('carrito');
