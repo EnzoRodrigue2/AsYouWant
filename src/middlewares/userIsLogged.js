a2341db6ada47867571d565f1f5561807516dd99
Adriel2323 <adrielcolombo23@gmail.com>
2022-05-13 17:34:17 -0300

    Acomode un problema con el homeUser, estaba en un bucle de redireccion que no cortaba. Solucionado

--- src/middlewares/userIsLogged.js
+++ src/middlewares/userIsLogged.js
@@ -1,7 +1,6 @@
 function userIsLogged (req, res, next) {
     if (req.session.usuarioLogueado){
         let usuarioLogueado = req.session.usuarioLogueado;
-        return res.redirect('/homeUser')
     }
     next();
 }
