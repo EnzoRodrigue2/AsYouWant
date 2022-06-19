384686fce947bf86a4e2bdbdd5d3019e0e46803c
Adriel23 <adrielcolombo23@gmail.com>
2022-06-19 03:43:38 -0300

    API de categorias completa y funcionando

--- src/Controllers/api/productsApiController.js
+++ src/Controllers/api/productsApiController.js
@@ -7,34 +7,76 @@ const productsController = {
     list: (req,res) => {
         db.Curso.findAll()
         .then(cursos => {
-            let response = {
-                count: cursos.length,
-                cursos: cursos
-            };
-            res.json(response)
+            if (cursos.length > 0){
+                let response = {
+                    count: cursos.length,
+                    cursos: cursos,
+                    status: 200
+                };
+                res.json(response)
+            } else {
+                res.json("No se encontraron cursos")
+            }
         }); 
+     },
+     categoria: (req, res) => {
+         let categoriaBuscada = req.params.categoria;
+         db.categoriaCursos.findAll()
+         .then(function(resultado){
+            let categoriaFiltro = "";
+            for(i=0;i<resultado.length;i++){
+                if(resultado[i].nombre == categoriaBuscada ){
+                    categoriaFiltro = resultado[i];
+                    break;
+                }
+            }
+            if (categoriaFiltro != "") {
+                db.Curso.findAll({
+                    where:{
+                        categoriaCursos_ID: categoriaFiltro.id
+                    }
+                })
+                .then(function(cursosFiltrados) {
+                    let response = {
+                        count: cursosFiltrados.length,
+                        cursos: cursosFiltrados,
+                        status: 200
+                    };
+                    res.json(response)
+                    
+                })
+            } else {
+                res.json("Todavia no tenemos cursos sobre eso")
+            }
+
+         })
      },
 
      detail: (req, res) => {
         db.Curso.findByPk(req.params.id)
         .then(curso => {
-            let response = {
-                titulo: curso.titulo,
-                descripcion: curso.descripcion,
-                descripcion_corta: curso.descripcion_corta,
-                precio: curso.precio,
-                unidades: curso.unidades,
-                audio: curso.audio,
-                vide: curso.video,
-                lectura: curso.lectura,
-                clases: curso.clases,
-                duracion: curso.duracion,
-                categoriaCursos: curso.categoriaCursos_ID,
-                proferor: curso.profesor_ID,
-                unidades_ID: curso.unidades_ID,
-                imagen: curso.imagen
-            };
-            res.json(response)
+            if (curso.length > 0 ) {
+                let response = {
+                    titulo: curso.titulo,
+                    descripcion: curso.descripcion,
+                    descripcion_corta: curso.descripcion_corta,
+                    precio: curso.precio,
+                    unidades: curso.unidades,
+                    audio: curso.audio,
+                    vide: curso.video,
+                    lectura: curso.lectura,
+                    clases: curso.clases,
+                    duracion: curso.duracion,
+                    categoriaCursos: curso.categoriaCursos_ID,
+                    proferor: curso.profesor_ID,
+                    unidades_ID: curso.unidades_ID,
+                    imagen: curso.imagen,
+                    status: 200
+                };
+                res.json(response)
+            } else {
+                res.json("No se encontro el curso buscado")
+            }
         });
      }
 
--- src/Routes/api/productsApiRoutes.js
+++ src/Routes/api/productsApiRoutes.js
@@ -6,6 +6,9 @@ const productsController = require('../../Controllers/api/productsApiController'
 /*Lista de productos*/
 router.get('/', productsController.list);
 
+/*Lista de productos por categoria*/
+router.get('/:categoria', productsController.categoria);
+
 /*Detalle de cada curso*/
 router.get('/:id', productsController.detail);
 
