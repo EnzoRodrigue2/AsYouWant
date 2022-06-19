195562379651eb44b637be06a16f5216327dc69c
Adriel23 <adrielcolombo23@gmail.com>
2022-06-18 22:33:35 -0300

    Validacion formulario de creacion del lado del frontend

--- public/Js/validacionFormCreacionProduct.js
+++ public/Js/validacionFormCreacionProduct.js
@@ -1,44 +1,43 @@
-// window.addEventListener("load", function() {
+window.addEventListener("load", function() {
 
-//     let formulario = document.forms["crear_form"];
+    let formulario = document.querySelector("form.crear_form");
 
+    formulario.addEventListener("submit", function(evento){
+        
+        let errors =[];
+        let campoTitulo = document.querySelector('input.titulo');
+        if (campoTitulo.value == '') {
+        errors.push("El campo de titulo esta vacio")
+        };
+        console.log(campoTitulo);
 
-//     formulario.addEventListened("submit", function(e) {});
+        let campoDescripcionCorta = document.querySelector('input.curso_descripcionCorta');
+        if (campoDescripcionCorta.value == '') {
+        errors.push("El campo de descripcion esta vacio")
+        };
+        console.log(campoDescripcionCorta);
 
-//     let errors =[];
+        let campoImagen = document.querySelector('input.curso_imagen');
+        if (campoImagen.value == '') {
+        errors.push("Debes colocar una imagen")
+        };
+        console.log(campoImagen);
 
-//     let campoTitulo = document.querySelector('input.titulo');
-//     if (campoTitulo.value == '') {
-//     errors.push("El campo de titulo esta vacio")
-//     };
-//     console.log(campoTitulo);
+        let campoPrecio = document.querySelector('input.curso_precio');
+        if (campoPrecio.value == '') {
+        errors.push("Debes colocarle un precio al curso")
+        };
+        console.log(campoPrecio);
 
-//     let campoDescripcionCorta = document.querySelector('input.curso_descripcionCorta');
-//     if (campoDescripcionCorta.value == '') {
-//     errors.push("El campo de descripcion esta vacio")
-//     };
-//     console.log(campoDescripcionCorta);
-
-//     let campoImagen = document.querySelector('input.curso_imagen');
-//     if (campoImagen.value == '') {
-//     errors.push("Debes colocar una imagen")
-//     };
-//     console.log(campoImagen);
-
-//     let campoPrecio = document.querySelector('input.curso_precio');
-//     if (campoPrecio.value == '') {
-//     errors.push("Debes colocarle un precio al curso")
-//     };
-//     console.log(campoPrecio);
-
-//     if (errors.length > 0) {
-//         e.preventDefault();
-//         let ulErrors = document.querySelector("div.errores ul");
-//         for (i = 0; i < errors.length; i++) {
-//             ulErrors.innerHTML += "<li> + errors[i] + </li>";
+        if (errors.length > 0) {
+            evento.preventDefault();
+            let ulErrors = document.getElementById("crear-errorList");
+            console.log(ulErrors);
+            for (i = 0; i < errors.length; i++) {
+                ulErrors.innerHTML += "<li>"+errors[i]+"</li>";
             
-//         }
-//     }
-// })
-
-alert(1);
+            }
+        }
+        })
+    
+})
