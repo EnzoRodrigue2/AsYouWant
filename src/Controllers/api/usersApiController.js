812171b176f57f41586b0322753c9b13fa50557c
Pauli019 <paulimonjes@gmail.com>
2022-06-25 12:50:14 -0300

    Arreglé homeUser, API de categoria de usuarios, otra base de datos... etc

--- src/Controllers/api/usersApiController.js
+++ src/Controllers/api/usersApiController.js
@@ -6,11 +6,12 @@ const Op = sequelize.Op;
 const usersController = {
 
     list: (req,res) => {
-        db.Usuario.findAll({attributes: ['id', 'nombre', 'apellidos', 'email', 'detail']})
+        db.Usuario.findAll({attributes: ['id', 'nombre', 'apellido', 'email', 'detail']})
         .then(usuarios => {
             let response = {
                 count: usuarios.length,
-                users: usuarios
+                users: usuarios,
+                status: 200
             };
             res.json(response)
         }); 
@@ -21,10 +22,39 @@ const usersController = {
         .then(user => {
             let response = {
                 nombre: user.nombre,
-                apellidos: user.apellidos,
+                apellido: user.apellido,
                 email: user.email,
                 descripcion: user.descripcion,
-                imagen: '/Images/users/' + user.imagen
+                imagen: '/Images/users/' + user.imagen,
+                status: 200
+            };
+            res.json(response)
+        });
+     },
+     
+     category: (req, res) => {
+        db.Categoria.findAll({attributes: ['id', 'nombre']})
+        .then(categories => {
+            let response = {
+                count: categories.length,
+                categorias: categories,
+                status: 200
+            };
+            res.json(response)
+        });
+     },
+
+     detCategory: (req, res) => {
+        let categoriaId = req.params.id;
+        db.Usuario.findAll({
+            where: {
+                categoria_ID: categoriaId
+            }
+        })
+        .then(users => {
+            let response = {
+                count: users.length,
+                status: 200
             };
             res.json(response)
         });
